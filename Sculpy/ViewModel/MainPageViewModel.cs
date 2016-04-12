13c6a018a06a165c66f75e883d8212563375934d
Casper Nielsen <Casp674d@edu.easj.dk>
2016-04-12 13:00:15 +0200

    Map

--- Sculpy/ViewModel/MainPageViewModel.cs
+++ Sculpy/ViewModel/MainPageViewModel.cs
@@ -1,10 +1,18 @@
-﻿namespace Sculpy.ViewModel
+﻿using Windows.Devices.Geolocation;
+
+namespace Sculpy.ViewModel
 {
     public class MainPageViewModel
     {
         public MainPageViewModel()
         {
-            
+            BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 47.604, Longitude = -122.329 };
+            Geopoint cityCenter = new Geopoint(cityPosition);
+
+            //// Set the map location.
+            //MapControl1.Center = cityCenter;
+            //MapControl1.ZoomLevel = 12;
+            //MapControl1.LandmarksVisible = true;
         }
     }
 }
