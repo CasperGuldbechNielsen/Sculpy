668f0cdb4012976ffc411d774a4c0e838dfda263
Casper Nielsen <Casp674d@edu.easj.dk>
2016-04-12 12:50:53 +0200

    Initial Commit

--- Sculpy/App.xaml.cs
+++ Sculpy/App.xaml.cs
@@ -28,6 +28,9 @@ namespace Sculpy
         /// </summary>
         public App()
         {
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
