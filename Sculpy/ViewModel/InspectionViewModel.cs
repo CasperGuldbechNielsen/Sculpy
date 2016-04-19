74b20e928605ef6918395b28c9791cc9ffb14a1f
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-19 11:09:42 +0200

    Implemented INotifyPropertyChanged to InspectionViewModel

--- Sculpy/ViewModel/InspectionViewModel.cs
+++ Sculpy/ViewModel/InspectionViewModel.cs
@@ -1,7 +1,17 @@
-﻿namespace Sculpy.ViewModel
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Sculpy.Annotations;
+
+namespace Sculpy.ViewModel
 {
-    public class InspectionViewModel
+    public class InspectionViewModel:INotifyPropertyChanged
     {
-         
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
