945e06d0d9162cfee2b2ad77a54d155694773aec
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-21 19:01:11 +0200

    Added title property to Inspection class

--- Sculpy/Model/InspectionCatalogSingleton.cs
+++ Sculpy/Model/InspectionCatalogSingleton.cs
@@ -26,15 +26,15 @@ namespace Sculpy.Model
         {
             Inspections = new ObservableCollection<Inspection>
             {
-                new Inspection(1, "24/03/2015", "Lorem Ipsum aaaaaaaaaaaaaaaaal the way. You not it!", 1, "Null"),
-                new Inspection(1, "12/10/2014", "Lorem Ipsum aaaaaaaaaaaaaaaaal the way. You not it!", 1, "Null"),
-                new Inspection(1, "02/01/2012", "Lorem Ipsum aaaaaaaaaaaaaaaaal the way. You not it!", 1, "Null"),
+                new Inspection(1, "24/03/2015", "General inspection", "Lorem Ipsum aaaaaaaaaaaaaaaaal the way. You not it!", 1, "Null"),
+                new Inspection(1, "12/10/2014", "Follow-up inspection", "Lorem Ipsum aaaaaaaaaaaaaaaaal the way. You not it!", 1, "Null"),
+                new Inspection(1, "02/01/2012", "General inspection", "Lorem Ipsum aaaaaaaaaaaaaaaaal the way. You not it!", 1, "Null"),
             };
         }
 
-        public void Add(int iD, string inspection_Date, string inspection_Note, int sculpture_ID, string damage_Picture)
+        public void Add(int iD, string title, string inspection_Date, string inspection_Note, int sculpture_ID, string damage_Picture)
         {
-            Inspections.Add(new Inspection(iD, inspection_Date, inspection_Note, sculpture_ID, damage_Picture));
+            Inspections.Add(new Inspection(iD, title, inspection_Date, inspection_Note, sculpture_ID, damage_Picture));
         }
     }
 }
