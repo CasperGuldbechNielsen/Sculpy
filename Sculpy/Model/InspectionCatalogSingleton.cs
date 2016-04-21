99362b6ded975a66f6ee2d5a905dd70fd1541d26
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-21 18:43:27 +0200

    Added ObservableCollection of inspections for testing

--- Sculpy/Model/InspectionCatalogSingleton.cs
+++ Sculpy/Model/InspectionCatalogSingleton.cs
@@ -24,7 +24,12 @@ namespace Sculpy.Model
         /// </summary>
         private void LoadInspections()
         {
-            //Inspections = new ObservableCollection<Inspection>(new PersistenceFacade().GetAllInspections());
+            Inspections = new ObservableCollection<Inspection>
+            {
+                new Inspection(1, "24/03/2015", "Lorem Ipsum aaaaaaaaaaaaaaaaal the way. You not it!", 1, "Null"),
+                new Inspection(1, "12/10/2014", "Lorem Ipsum aaaaaaaaaaaaaaaaal the way. You not it!", 1, "Null"),
+                new Inspection(1, "02/01/2012", "Lorem Ipsum aaaaaaaaaaaaaaaaal the way. You not it!", 1, "Null"),
+            };
         }
 
         public void Add(int iD, string inspection_Date, string inspection_Note, int sculpture_ID, string damage_Picture)
