e4e4723795867037637b935f789d0edca786365e
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-21 10:29:21 +0200

    New initial commit ;)

--- Sculpy/App.xaml.cs
+++ Sculpy/App.xaml.cs
@@ -73,7 +73,7 @@ namespace Sculpy
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(Main), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
