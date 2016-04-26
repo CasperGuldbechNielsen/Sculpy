81b9de14b03a9ca805909069f0025a5be7f24472
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-26 13:12:49 +0200

    Added map and centered location on Copenhagen

--- Sculpy/View/MapView.xaml.cs
+++ Sculpy/View/MapView.xaml.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
@@ -25,6 +28,32 @@ namespace Sculpy.View
         public MapView()
         {
             this.InitializeComponent();
+            sculptureMap.Loaded += SculptureMap_OnLoaded;
+        }
+
+        private async void SculptureMap_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var center =
+                new Geopoint(new BasicGeoposition()
+                {
+                    Latitude = 55.67437,
+                    Longitude = 12.56751,
+                });
+            await sculptureMap.TrySetSceneAsync(MapScene.CreateFromLocationAndRadius(center, 3500));
+
+            //Testing pushpins...
+            var pushpin = new MapIcon();
+
+            pushpin.Location = new Geopoint(new BasicGeoposition()
+            {
+                Latitude = 55.67573,
+                Longitude = 12.56858
+            });
+
+            pushpin.Title = "Dragespringvandet";
+
+            sculptureMap.MapElements.Add(pushpin);
+
         }
     }
 }
