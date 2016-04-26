6d0a23217089a97e51df810f2a375036c7869105
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-26 10:19:32 +0200

    Deleted unused usercontrols

--- Sculpy/View/Sculptures1.xaml.cs
+++ Sculpy/View/Sculptures1.xaml.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,7 +12,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Microsoft.Xaml.Interactions.Core;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -24,11 +22,7 @@ namespace Sculpy.View
         public Sculptures()
         {
             this.InitializeComponent();
-
-            var frameworkElement = this.Content as FrameworkElement;
-            if (frameworkElement != null) frameworkElement.DataContext = this;
         }
-
         private void FilterButton_OnClick(object sender, RoutedEventArgs e)
         {
             FilterWindow.Visibility = FilterWindow.Visibility == Visibility.Collapsed
@@ -36,7 +30,7 @@ namespace Sculpy.View
                 : Visibility.Collapsed;
 
             FilterButton.Foreground = FilterWindow.Visibility == Visibility.Collapsed
-                ? FilterButton.Foreground = new SolidColorBrush(Colors.White)
+                ? SortButton.Foreground
                 : FilterWindow.BorderBrush;
         }
 
@@ -47,18 +41,8 @@ namespace Sculpy.View
                 : Visibility.Collapsed;
 
             SortButton.Foreground = SortingWindow.Visibility == Visibility.Collapsed
-               ? SortButton.Foreground = new SolidColorBrush(Colors.White)
+               ? FilterButton.Foreground
                : SortingWindow.BorderBrush;
         }
-        
-
-        private void SculptureListView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            
-
-
-        }
-
-        
     }
 }
