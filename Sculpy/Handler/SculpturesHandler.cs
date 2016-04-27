ab3269854b7bb53934f1065c9a11a4b2db6a7eee
Daniel-Simionescu <simionescu_dany@yahoo.com>
2016-04-27 16:05:25 +0200

    Added the sorted collection

---
+++ Sculpy/Handler/SculpturesHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sculpy.Model;
+using Sculpy.ViewModel;
+using static Sculpy.ViewModel.SculpturesViewModel;
+
+namespace Sculpy.Handler
+{
+    public static class SculpturesHandler
+    {
+        public static ObservableCollection<Sculpture> FilterCollection()
+        {
+            //FilteredSortedCollection = FilteredSortedCollection.Where(x => x.Sculpture_Placement == "Building").ToList();
+
+            return null;
+        }
+    }
+}
---
+++ Sculpy/ViewModel/SculpturesViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sculpy.Model;
+
+namespace Sculpy.ViewModel
+{
+    class SculpturesViewModel
+    {
+        public SculptureCatalogSingleton SculptureCatalogSingleton { get; set; }
+
+        public static ObservableCollection<Sculpture> FilteredSortedCollection { get; set; }
+
+
+        public SculpturesViewModel()
+        {
+            SculptureCatalogSingleton = SculptureCatalogSingleton.Instance;
+
+            FilteredSortedCollection = SculptureCatalogSingleton.Instance.Sculptures;
+        }
+
+
+
+
+    }
+}
