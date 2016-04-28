eb88fc7f650a789d7efce1ec08cf2a0bc4f668e7
Casper Nielsen <Casp674d@edu.easj.dk>
2016-04-28 10:44:22 +0200

    Added a report handler

---
+++ Sculpy/Handler/ReportHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using Sculpy.Model;
+
+namespace Sculpy.Handler
+{
+    public class ReportHandler
+    {
+        private static SculptureCatalogSingleton CatalogSingleton { get; } = SculptureCatalogSingleton.Instance;
+
+        internal static void FilterCollectionByPeriod(string periodFilter)
+        {
+            var filteredCollection =
+                            CatalogSingleton.Sculptures.Where(x => x.Sculpture_Placement == periodFilter).ToList();
+
+            CatalogSingleton.Sculptures.Clear();
+
+            foreach (var sculpture in filteredCollection)
+            {
+                CatalogSingleton.Sculptures.Add(sculpture);
+            }
+        }
+
+        internal static void FilterCollectionByDamage(string damageFilter)
+        {
+            var filteredCollection =
+                            CatalogSingleton.Sculptures.Where(x => x.Sculpture_Placement == damageFilter).ToList();
+
+            CatalogSingleton.Sculptures.Clear();
+
+            foreach (var sculpture in filteredCollection)
+            {
+                CatalogSingleton.Sculptures.Add(sculpture);
+            }
+        }
+
+        internal static void FilterCollectionByTreatmentFrequency(string treatmentFrequencyFilter)
+        {
+            var filteredCollection =
+                            CatalogSingleton.Sculptures.Where(x => x.Sculpture_Placement == treatmentFrequencyFilter).ToList();
+
+            CatalogSingleton.Sculptures.Clear();
+
+            foreach (var sculpture in filteredCollection)
+            {
+                CatalogSingleton.Sculptures.Add(sculpture);
+            }
+        }
+
+        internal static void FilterCollectionBySuggestedTreatment(string suggestedTreatmentFilter)
+        {
+            var filteredCollection =
+                            CatalogSingleton.Sculptures.Where(x => x.Sculpture_Placement == suggestedTreatmentFilter).ToList();
+
+            CatalogSingleton.Sculptures.Clear();
+
+            foreach (var sculpture in filteredCollection)
+            {
+                CatalogSingleton.Sculptures.Add(sculpture);
+            }
+        }
+    }
+}
--- Sculpy/ViewModel/ReportViewModel.cs
+++ Sculpy/ViewModel/ReportViewModel.cs
@@ -1,19 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+using Sculpy.Annotations;
+using Sculpy.Handler;
 using Sculpy.Model;
 
 namespace Sculpy.ViewModel
 {
-    class ReportViewModel
+    class ReportViewModel : INotifyPropertyChanged
     {
         public InspectionCatalogSingleton inspectionCatalogSingleton { get; set; }
 
         public ReportViewModel()
         {
             inspectionCatalogSingleton = InspectionCatalogSingleton.Instance;
+        }
+
+        private string _periodFilter;
+        public ComboBoxItem PeriodFilter
+        {
+            set
+            {
+                _periodFilter = value?.Tag?.ToString();
+                ReportHandler.FilterCollectionByPeriod(_periodFilter);
+            }
+        }
+
+        private string _suggestedTreatmentFilter;
+        public ComboBoxItem SuggestedTreatmentFilter
+        {
+            set
+            {
+                _suggestedTreatmentFilter = value?.Tag?.ToString();
+                ReportHandler.FilterCollectionBySuggestedTreatment(_suggestedTreatmentFilter);
+            }
+        }
+
+        private string _treatmentFrequencyFilter;
+        public ComboBoxItem TreatmentFrequencyFilter
+        {
+            set
+            {
+                _treatmentFrequencyFilter = value?.Tag?.ToString();
+                ReportHandler.FilterCollectionByTreatmentFrequency(_treatmentFrequencyFilter);
+            }
+        }
+
+        private string _damageFilter;
+        public ComboBoxItem DamageFilter
+        {
+            set
+            {
+                _damageFilter = value?.Tag?.ToString();
+                ReportHandler.FilterCollectionByDamage(_damageFilter);
+            }
+        }
+
+
+
+        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
