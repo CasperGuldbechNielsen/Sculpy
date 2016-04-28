7d79e76f8e8d75b59d4b0d8d538a83a9159f2d32
Daniel-Simionescu <simionescu_dany@yahoo.com>
2016-04-28 10:23:40 +0200

    commit

--- Sculpy/ViewModel/ReportViewModel.cs
+++ Sculpy/ViewModel/ReportViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Sculpy.Annotations;
+using Sculpy.Model;
 
 namespace Sculpy.ViewModel
 {
     class ReportViewModel
     {
+       
     }
 }
