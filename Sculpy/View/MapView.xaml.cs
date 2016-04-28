aab816142d764d2918172fb9bd77fde6e6eaacee
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-04-28 20:45:00 +0200

    Added functionality to find users current location

--- Sculpy/View/MapView.xaml.cs
+++ Sculpy/View/MapView.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -25,6 +26,11 @@ namespace Sculpy.View
     /// </summary>
     public sealed partial class MapView : Page
     {
+
+        private CancellationTokenSource _cts = null;
+        private double _latitude = 55.67610;
+        private double _longitude = 12.56834;
+
         public MapView()
         {
             this.InitializeComponent();
@@ -35,8 +41,8 @@ namespace Sculpy.View
             var center =
                 new Geopoint(new BasicGeoposition()
                 {
-                    Latitude = 55.67610,
-                    Longitude = 12.56834
+                    Latitude = _latitude,
+                    Longitude = _longitude
                 });
             await sculptureMap.TrySetSceneAsync(MapScene.CreateFromLocationAndRadius(center, 3500), MapAnimationKind.Bow);
 
@@ -45,6 +51,63 @@ namespace Sculpy.View
         private void SettingsButton_OnClick(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(SettingsView));
+        }
+
+        private async void GetGeolocationButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            GetGeolocationButton.IsEnabled = false;
+
+            try
+            {
+                // Request permission to access location
+                var accessStatus = await Geolocator.RequestAccessAsync();
+
+                switch (accessStatus)
+                {
+                    case GeolocationAccessStatus.Allowed:
+
+                        // Get cancellation token
+                        _cts = new CancellationTokenSource();
+                        CancellationToken token = _cts.Token;
+
+                        StatusMessage.Visibility = Visibility.Visible;
+
+                        Geolocator geolocator = new Geolocator();
+
+                        // Carry out the operation
+                        Geoposition pos = await geolocator.GetGeopositionAsync().AsTask(token);
+                        UpdateLocationData(pos);
+                        StatusMessage.Visibility = Visibility.Collapsed;
+                        break;
+
+                    case GeolocationAccessStatus.Denied:
+                        break;
+
+                    case GeolocationAccessStatus.Unspecified:
+                        break;
+                }
+            }
+            finally
+            {
+                _cts = null;
+            }
+
+            GetGeolocationButton.IsEnabled = true;
+        }
+
+        private async void UpdateLocationData(Geoposition position)
+        {
+            _latitude = position.Coordinate.Point.Position.Latitude;
+            _longitude = position.Coordinate.Point.Position.Longitude;
+
+            var center =
+                new Geopoint(new BasicGeoposition()
+                {
+                    Latitude = _latitude,
+                    Longitude = _longitude
+                });
+            await
+                sculptureMap.TrySetSceneAsync(MapScene.CreateFromLocationAndRadius(center, 200), MapAnimationKind.Bow);
         }
     }
 }
