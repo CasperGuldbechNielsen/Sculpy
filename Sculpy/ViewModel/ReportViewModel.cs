d8cf86d6318ec81df17be4390e25baaf90338a14
Casper Nielsen <Casp674d@edu.easj.dk>
2016-04-28 10:12:41 +0200

    Added InspectionCatalogSingleton instance

--- Sculpy/ViewModel/ReportViewModel.cs
+++ Sculpy/ViewModel/ReportViewModel.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sculpy.Model;
 
 namespace Sculpy.ViewModel
 {
     class ReportViewModel
     {
+        InspectionCatalogSingleton inspectionCatalogSingleton { get; set; }
+        public ReportViewModel()
+        {
+            inspectionCatalogSingleton = InspectionCatalogSingleton.Instance;
+        }
     }
 }
