d1e274ad4be153d995b7417b66d012d9213fc2bd
Casper Nielsen <Casp674d@edu.easj.dk>
2016-04-28 10:10:18 +0200

    Added reportViewModel

---
+++ Sculpy/ViewModel/ReportViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Sculpy.Model;
+
+namespace Sculpy.ViewModel
+{
+    public class ReportViewModel
+    {
+        InspectionCatalogSingleton inspectionCatalogSingleton { get; set; }
+        public ReportViewModel()
+        {
+            inspectionCatalogSingleton = InspectionCatalogSingleton.Instance;
+        } 
+    }
+}
