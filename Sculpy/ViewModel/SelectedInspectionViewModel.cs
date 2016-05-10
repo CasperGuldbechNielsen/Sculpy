c9fe778e6a64c1418eb77e370fd8ff9d43953c87
ystvan <istv0050@edu.easj.dk>
2016-05-10 16:06:02 +0200

    setting up viewmodel setting up xaml setters

--- Sculpy/ViewModel/SelectedInspectionViewModel.cs
+++ Sculpy/ViewModel/SelectedInspectionViewModel.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Sculpy.Annotations;
+using Sculpy.Handler;
+using Sculpy.Model;
 
 namespace Sculpy.ViewModel
 {
-    class SelectedInspectionViewModel
+    class SelectedInspectionViewModel:INotifyPropertyChanged
     {
+
+        private Inspection _passedInspection;
+
+        public Inspection PassedInspection
+        {
+            get { return _passedInspection;}
+            set
+            {
+                _passedInspection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public InspectionHandler Handler { get; }
+
+        public SelectedInspectionViewModel()
+        {
+            
+        }
+
+        public SelectedInspectionViewModel(InspectionHandler handler)
+        {
+            this.Handler = handler;
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
