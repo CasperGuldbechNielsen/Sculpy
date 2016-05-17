489ebc23d61984d2090c3bc77c5aa11b30b51c8e
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-05-17 10:41:40 +0200

    Rest of it

---
+++ WebService/Controllers/InspectionForSculptureController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace WebService.Controllers
+{
+    public class InspectionForSculptureController : ApiController
+    {
+        SculptureContext db = new SculptureContext();
+
+        [Route("api/CreateInspectionForSculpture/{sculptureId:int}")]
+        [HttpPut]
+        [ResponseType(typeof(void))]
+        public IHttpActionResult CreateInspectionForSculpture(int sculptureId, Inspection inspection)
+        {
+          
+            inspection.Sculpture_ID = sculptureId;
+            db.Inspections.Add(inspection);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
